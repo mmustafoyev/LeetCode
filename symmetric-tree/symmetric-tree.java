0c60d54cd8010044ef7ffa0b200385189d32cd25
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2023-11-28 19:45:52 +0500

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -15,16 +15,20 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-        return toCheck(root.left, root.right);
+        if(root == null) return true;
+        if(root.left != null && root.right != null)
+            return toCheck(root.left, root.right);
+        else if (root.left == null && root.right == null)
+            return true;
+        return false;
     }
-
-    private boolean toCheck(TreeNode left, TreeNode right) {
-        if (left == null && right == null) return true;
-        else if (left == null || right == null) return false;
-    
-        if (left.val != right.val) return false;
-        // Explicitly return the results of recursive calls
+    public boolean toCheck(TreeNode left, TreeNode right){
+        if((left != null && right == null) || (left == null && right != null))
+           return false;
+        else if(left == null && right == null)
+            return true;
+        if(left.val != right.val) 
+            return false;
         return toCheck(left.left, right.right) && toCheck(left.right, right.left);
     }
 }
