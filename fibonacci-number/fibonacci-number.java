6342a31d951fbddb008d6dec0811f8f96e914f4b
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2023-12-29 23:26:44 +0500

    Time: 0 ms, Memory: 40.7 MB - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,7 +1,20 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        else if(n == 1) return 1;
-        else return fib(n - 1) + fib(n - 2);
+        int a=0;
+        int b=1;
+        if(n==0)
+            return a;
+        if(n==1)
+            return b;
+        if(n==2)
+            return a+b;
+        int c=a+b;
+        for(int i=2;i<n;i++)
+        {
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        return c;
     }
 }
