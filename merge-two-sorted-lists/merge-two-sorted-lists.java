c90d6b654708cd0a3775dbe6532f051dce546c02
Muhammad Mustafoyev <135558935+mmustafoyev@users.noreply.github.com>
2024-01-05 04:14:48 +0500

    Time: 0 ms, Memory: 42.5 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,7 +10,21 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy  = new ListNode(0);
+        if(list1 == null)
+            return list2;
+        else if(list2 == null)
+            return list1;
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
+/*ListNode dummy  = new ListNode(0);
         ListNode current = dummy;
     
         while(list1 != null && list2 != null){
@@ -28,6 +42,4 @@ class Solution {
             current.next = list1;
         if(list2 != null)
             current.next = list2;
-        return dummy.next;
-    }
-}
+        return dummy.next;*/
